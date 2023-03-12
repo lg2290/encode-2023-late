f0cd82d1a2fc6b3ca86089a8115dd496980b7844
Luis Gustavo Garcia de Carvalho <luis.carvalho@bitso.com>
2023-03-11 23:51:57 -0300

    Removing unused code

--- weekend-4/weekend-4-backend/src/app.module.ts
+++ weekend-4/weekend-4-backend/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BallotController } from './ballot/ballot.controller';
 import { BallotService } from './ballot/ballot.service';
 import { MyTokenController } from './my-token/myToken.controller';
@@ -10,7 +8,7 @@ import { SignerService } from './signer/signer.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, BallotController, MyTokenController],
-  providers: [AppService, BallotService, MyTokenService, SignerService],
+  controllers: [BallotController, MyTokenController],
+  providers: [BallotService, MyTokenService, SignerService],
 })
 export class AppModule {}
