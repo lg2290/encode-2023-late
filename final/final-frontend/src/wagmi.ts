ca9f7d9f00686676ef30bdb4c2cf09acf71e610b
Luis Gustavo Garcia de Carvalho <luis.carvalho@bitso.com>
2023-04-03 00:09:28 -0300

    Adding page to create challenge

--- final/final-frontend/src/wagmi.ts
+++ final/final-frontend/src/wagmi.ts
@@ -3,7 +3,7 @@ import { configureChains, createClient } from 'wagmi'
 import { goerli, mainnet } from 'wagmi/chains'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const walletConnectProjectId = 'ADD'
+export const walletConnectProjectId = '561ad3cf4d7e96b0d15f55d4477cb8f1'
 
 const { chains, provider, webSocketProvider } = configureChains(
   [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
