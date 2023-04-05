99219f5e84ff5edb1e51740510cb02b3e7404832
Luis Gustavo Garcia de Carvalho <luis.carvalho@bitso.com>
2023-04-04 23:32:06 -0300

    Changing text

--- final/final-frontend/src/pages/index.tsx
+++ final/final-frontend/src/pages/index.tsx
@@ -30,21 +30,13 @@ export default function Page(){
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-white">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
               Crypto Price Prediction Challenge
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-300">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+              Create your price prediction for the main cryptocurrencies, and let other person challenge it.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
